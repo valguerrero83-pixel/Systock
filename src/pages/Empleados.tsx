b1dc6927f13f29301b25e8f914b504df9fd65847
TuNombre <tuemail@example.com>
2026-02-09 14:43:02 -0500

    modales6

--- src/pages/Empleados.tsx
+++ src/pages/Empleados.tsx
@@ -116,7 +116,7 @@ export default function Empleados() {
 
       {loading && (
         <p className="text-center text-gray-500 mt-4 animate-pulse">
-          Cargando empleados…
+          Cargando empleados
         </p>
       )}
     </motion.div>
